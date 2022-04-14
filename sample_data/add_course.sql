e5897030e7674617325afc985740ac8cdd360f36
akash <akashsinha544@gmail.com>
2022-04-14 14:13:29 +0530

    adding sample_data

---
+++ sample_data/add_course.sql
@@ -0,0 +1,5 @@
+INSERT INTO `univ`.`course` (`course_instructor`, `course_name`, `course_video_url`, `course_notes_url`, `course_price`, `createdAt`, `updatedAt`) 
+VALUES 
+('Rahul ', 'Java Basics', 'video_url', 'notes_url', '5000', current_timestamp(3), current_timestamp(3)),
+('Bijay', 'DSA', 'video_url2', 'notes_url2', '6000', current_timestamp(3), current_timestamp(3)),
+('Rajiv', 'Javascript', 'video_url3', 'notes_url3', '8000', current_timestamp(3), current_timestamp(3));
---
+++ sample_data/add_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `univ`.`user` (`first_name`, `last_name`, `address`, `mobile`, `createdAt`, `updatedAt`)
+VALUES 
+('Akash', 'Sinha', 'Kharagpur', '765432', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+('Bikas', 'Rana', 'Bangalore', '7864378', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+('Bipul', 'Saha', 'Pune', '785432', current_timestamp(3), current_timestamp(3));
