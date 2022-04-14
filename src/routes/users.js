410c7b10bc035736aa75ca6e90b977a6608e1c0b
akash <akashsinha544@gmail.com>
2022-04-14 17:10:33 +0530

    search user by id

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,5 +8,16 @@ router.get('/', async (req, res) => {
     res.json(users)
 })
 
+router.get('/:id', async (req, res) =>{
+    console.log("trying to get user by id")
+    const {id} = req.params
+    console.log("the id is ", id)
+    let myUser = await user.findFirst({
+        where:{
+            id: Number(id)
+        },
+    })
+    res.json(myUser)
+})
 
 module.exports = router
